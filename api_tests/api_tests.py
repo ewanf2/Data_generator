8fc4fea31e9a4c695401f153b9db296556ce02cb
Ewan Fraser <ewanf@outlook.com>
2025-07-22 10:25:27 +0100

    fix import issues

--- api_tests/api_tests.py
+++ api_tests/api_tests.py
@@ -1,5 +1,10 @@
 from App import data_gen, docGenerator
 import re
+import sys
+import os
+
+# Add the project root to the import path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from playwright.sync_api import Playwright, APIRequest, APIRequestContext
 from typing import Generator
 import pytest
--- unittests/tests.py
+++ unittests/tests.py
@@ -1,4 +1,9 @@
-#Unit tests and dat
+
+import sys
+import os
+
+# Add the project root to the import path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from App import data_gen, docGenerator, docGenerator_simple
 import re
 from playwright.sync_api import Playwright, APIRequest
